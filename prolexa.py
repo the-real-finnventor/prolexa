fa72f3b301b6ad87173303c4cea7967ce069f440
Real Finnventor <realfinnventor@Finns-MacBook-Air.local>
2023-03-31 17:26:07 -0500

    added program

---
+++ prolexa.py
@@ -0,0 +1,22 @@
+# Import the modules
+import subprocess
+import whisper
+import openai
+# Load the modal
+model = whisper.load_model("base")
+# Ask "How can I help you?"
+subprocess.run(['say', 'how can i help you'])
+# Record to the user and save the recording to a new file called said.mp3
+subprocess.run(['ffmpeg', '-y', '-f', 'avfoundation', '-i', ':4', '-t', '10', 'said.mp3'])
+# Transcribe the audio file
+result = model.transcribe("said.mp3")
+# Print out what they said
+print(result["text"])
+# Add you open-ai key (that you can get at https://platform.openai.com/account/api-keys)
+openai.api_key = 'sk-xxx' # Put your key inside the single quotes
+# Have ChatGPT provide an answer to the question
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": result["text"]}])
+# Print out the answer
+print(completion.choices[0].message.content)
+# Play the answer out of the speaker
+subprocess.run(['say', completion.choices[0].message.content])
